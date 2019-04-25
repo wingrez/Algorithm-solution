a39ca74afa88ec3c8f3a5e911af2671a3574e0ee
Wingrez <31106425+wingrez@users.noreply.github.com>
2019-04-25 12:38:28 +0800

    Update 17.cpp

--- NYOJ/17.cpp
+++ NYOJ/17.cpp
@@ -1,49 +1,28 @@
 #include<stdio.h>
 #include<string.h>
-#include<vector>
-#define MAXN 100000+5
+#include<algorithm>
+#define MAXN 100005
 using namespace std;
-
-int n,s;
-vector<int> G[MAXN];
-int p[MAXN];
-
-void tree(int u,int v){
-	G[u].push_back(v);
-	G[v].push_back(u);
-}
-
-void dfs(int u,int fa){//递归生成u的子树，fa为u的父结点。 
-	int d=G[u].size();
-	for(int i=0;i<d;i++){
-		int v=G[u][i];
-		if(v!=fa){
-			dfs(v,p[v]=u);
-		}
-	}
-}
-
+char str[MAXN];
+int dp[MAXN];
 int main(){
-	int m;
-	scanf("%d",&m);
-	while(m--){
-		for(int i=0;i<MAXN;i++){
-			G[i].clear();
+	int n;
+	scanf("%d",&n);
+	while(n--){
+		scanf("%s",str);
+		int len=strlen(str);
+		int ans=0;
+		for(int i=0;i<len;i++){
+			dp[i]=1;
+			for(int j=0;j<i;j++){
+				if(str[j]<str[i]){
+					dp[i]=max(dp[i],dp[j]+1);
+				}
+			}
+			ans=max(ans,dp[i]);
 		}
-		scanf("%d%d",&n,&s);
-		int a,b;
-		for(int i=0;i<n-1;i++){
-			scanf("%d%d",&a,&b);
-			tree(a,b);
-		}
-		p[s]=-1;
-		dfs(s,-1);//生成以s为根的树
-		for(int i=1;i<=n;i++){
-			printf("%d ",p[i]);
-		}
-		printf("\n");
+		printf("%d\n",ans);
 	}
 	return 0;
 }
-
 
